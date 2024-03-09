2164e1ca315607da3c59f0f627c8dc61db0aa4bf
Juwon-Kiim <kimjuwon@gimjuwon-ui-MacBookAir.local>
2024-03-09 17:39:21 +0900

    test simple api

---
+++ OAuthJwt/src/main/java/com/example/OAuthJwt/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.OAuthJwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String mainApi(){
+        return "main route";
+    }
+}
